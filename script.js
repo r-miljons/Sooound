9b8ec376c7961b7ece0be4dd2ebfc3d0eb4aeab4
Renars Miljons <reku.miljons@gmail.com>
2022-08-06 11:39:51 +0300

    fixed import path for module

--- script.js
+++ script.js
@@ -760,14 +760,14 @@ if (document.querySelector('.product-img')) {
 
 // PhotoSwipeLightbox
 
-import PhotoSwipeLightbox from "../node_modules/photoswipe/dist/photoswipe-lightbox.esm.js";
+    import PhotoSwipeLightbox from "./node_modules/photoswipe/dist/photoswipe-lightbox.esm.js";
 
-const lightbox = new PhotoSwipeLightbox({
-	gallery: "#gallery--ampro2",
-	children: "a",
-	showHideAnimationType: "fade",
-	zoomAnimationDuration: false,
-	pswpModule: () => import("../node_modules/photoswipe/dist/photoswipe.esm.js"),
-});
+    const lightbox = new PhotoSwipeLightbox({
+    	gallery: "#gallery--ampro2",
+    	children: "a",
+    	showHideAnimationType: "fade",
+    	zoomAnimationDuration: false,
+    	pswpModule: () => import("./node_modules/photoswipe/dist/photoswipe.esm.js"),
+    });
 
-lightbox.init();
+    lightbox.init();
