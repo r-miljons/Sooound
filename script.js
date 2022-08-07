ddd4d474de21e8ff16880cf55b5230f180fb69b5
Renars Miljons <reku.miljons@gmail.com>
2022-08-07 09:29:49 +0300

    removed photoswipe

--- script.js
+++ script.js
@@ -759,12 +759,12 @@ if (document.querySelector('.product-img')) {
 
 // PhotoSwipeLightbox
 
-import PhotoSwipeLightbox from "./node_modules/photoswipe/dist/photoswipe-lightbox.esm.js";
-const lightbox = new PhotoSwipeLightbox({
-	gallery: "#gallery--ampro2",
-	children: "a",
-	showHideAnimationType: "fade",
-	zoomAnimationDuration: false,
-	pswpModule: () => import("./node_modules/photoswipe/dist/photoswipe.esm.js"),
-});
-lightbox.init();
+// import PhotoSwipeLightbox from 'photoswipe/dist/photoswipe-lightbox.esm.js';
+// const lightbox = new PhotoSwipeLightbox({
+// 	gallery: "#gallery--ampro2",
+// 	children: "a",
+// 	showHideAnimationType: "fade",
+// 	zoomAnimationDuration: false,
+// 	pswpModule: () => import('photoswipe/dist/photoswipe.esm.js')
+// });
+// lightbox.init();
